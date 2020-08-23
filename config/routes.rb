b3bef37af3d3e6e680ba1948863b250ba0a03dcf
Mark <markandrewhamm@gmail.com>
2020-08-23 11:25:54 -0500

    resolved merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,10 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/names" => "names#index"
-<<<<<<< HEAD
     
     get "/guitars" => "guitars#index"
 
-=======
     get "/brians" => "brians#index"
->>>>>>> 90d2b7607b0fb8989fdaa1718c4633d6f5feff81
   end
   
 end
