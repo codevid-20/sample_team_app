f625668162f67f5ff8286ec28fc6dd686448fa5e
melaniegrigg <melanie.grigger@gmail.com>
2020-08-23 12:25:50 -0400

    fix route typo for melanie

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/names" => "names#index"
-<<<<<<< HEAD
-    get "/melanie" +> "melanie#index"
-=======
+    get "/melanie" => "melanie#index"
     get "/brians" => "brians#index"
->>>>>>> 90d2b7607b0fb8989fdaa1718c4633d6f5feff81
   end
 end
